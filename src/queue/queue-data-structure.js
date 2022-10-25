b5dd0180858e2754658dd049062dad8148f455cd
Manuel Atance <ryuusei3@MacBook-Pro-de-Manuel.local>
2022-10-25 17:20:09 +0200

    Todo completado!

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -1,29 +1,46 @@
 class Queue {
-  constructor() {
-    this.queueControl = [];
-    this.MAX_SIZE = 10;
-  }
+	constructor() {
+		this.queueControl = [];
+		this.MAX_SIZE = 10;
+	}
 
-  canEnqueue() {
-    // ... your code goes here
-  }
+	canEnqueue() {
+		if (this.queueControl.length < this.MAX_SIZE) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-  isEmpty() {
-    // ... your code goes here
-  }
+	isEmpty() {
+		if (this.queueControl.length === 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-  enqueue(item) {
-    // ... your code goes here
-  }
+	enqueue(item) {
+		if (this.canEnqueue()) {
+			this.queueControl.push(item);
+			return this.queueControl;
+		} else {
+			throw new Error("QUEUE_OVERFLOW");
+		}
+	}
 
-  dequeue() {
-    // ... your code goes here
-  }
+	dequeue() {
+		if (this.isEmpty()) {
+			throw new Error("QUEUE_UNDERFLOW");
+		} else {
+			return this.queueControl.shift();
+		}
+	}
 
-  display() {
-    // ... your code goes here
-  }  
+	display() {
+		return this.queueControl;
+	}
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Queue;
+if (typeof module !== "undefined") module.exports = Queue;
